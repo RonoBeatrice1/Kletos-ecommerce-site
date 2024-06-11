d8d47fc8e703197093e9316258310ead7d14b743
RonoBeatrice1 <ronobc@gmail.com>
2024-06-11 15:09:44 +0300

    Made progress

--- src/components/productdetailscomponents/ProductImages.jsx
+++ src/components/productdetailscomponents/ProductImages.jsx
@@ -4,7 +4,7 @@ import "../../css/productdetailscss/productdetails.css";
 const ProductImages = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const thumbnails = [
-    "/home/hp/Kletos App2/public/images/1.jpg",
+    "/images/image-product-1-thumbnail.jpg",
     "/images/image-product-2-thumbnail.jpg",
     "/images/image-product-3-thumbnail.jpg",
     "/images/image-product-4-thumbnail.jpg",
