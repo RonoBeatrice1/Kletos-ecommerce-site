17e625153e29c2916d0c16f5110ed54aab3e88d8
RonoBeatrice1 <ronobc@gmail.com>
2024-06-12 11:54:34 +0300

    Adjusted the navbar code

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,40 +1,76 @@
 import "../../css/layoutcss/navbar.css";
 
-const NavBar = () => {
+// src/layout/NavBar.jsx
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faShoppingCart,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
   return (
-    <header>
-      <div className="left-header">
-        <button className="open-menu-btn">
-          <img className="menu" src="/images/icon-menu.svg" alt="menu-icon" />
-        </button>
-        <img className="logo" src="/images/logo.svg" alt="logo" />
+    <nav className="navbar">
+      <div className="navbar-section left-section">
+        <div className="logo">
+          <a href="/">Kletos</a>
+        </div>
+        <div className="menu">
+          <div className="menu-item">
+            <a href="/about">About</a>
+            <div className="sublinks">
+              <a href="/team">Team</a>
+              <a href="/history">History</a>
+            </div>
+          </div>
+          <div className="menu-item">
+            <a href="/shop">Shop</a>
+            <div className="sublinks">
+              <a href="/artifacts">Artifacts</a>
+              <a href="/crafts">Crafts</a>
+              <a href="/shields">Shields</a>
+              <a href="/calabashes">Calabashes</a>
+              <a href="/wood">Wood carvings and sculptures</a>
+              <a href="/bracelet">Bracelets</a>
+              <a href="/bags">Bags and mats</a>
+            </div>
+          </div>
+          <div className="menu-item">
+            <a href="/contact">Contact</a>
+          </div>
+        </div>
       </div>
-      <nav className="navigation-desktop">
-        <a href="#">Collections</a>
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#">About </a>
-        <a href="#">Contact</a>
-      </nav>
-      <nav className="navigation-mobile">
-        <button className="close-menu-btn">
-          <img src="/images/icon-close.svg" alt="close-menu-icon" />
-        </button>
-        <a href="#">Collections</a>
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#">About </a>
-        <a href="#">Contact</a>
-      </nav>
-      <div className="right-header">
-        <img className="cart" src="/images/icon-cart.svg" alt="cart" />
-        <span className="cart-count" id="cart-count">
-          0
-        </span>
-        <img className="avatar" src="/images/image-avatar.png" alt="avatar" />
+      <div className="navbar-section center-section">
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
       </div>
-    </header>
+      <div className="navbar-section right-section">
+        <div className="dropdown">
+          <a href="/profile">
+            <FontAwesomeIcon icon={faUser} /> Profile
+          </a>
+          <div className="dropdown-content">
+            <a href="/profile">View Profile</a>
+            <a href="/orders">Orders</a>
+            <a href="/logout">Logout</a>
+          </div>
+        </div>
+        <div className="dropdown">
+          <a href="/cart">
+            <FontAwesomeIcon icon={faShoppingCart} /> Cart
+          </a>
+          <div className="dropdown-content">
+            <a href="/cart">View Cart</a>
+            <a href="/checkout">Checkout</a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
